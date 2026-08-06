1a89519104c078ce5d8ac1530b7b9952a8e6208f
Ochab <greg@ochab.us>
2026-08-06 15:26:06 -0500

    Building button component, separated component styles

---
+++ src/components/Button.tsx
@@ -0,0 +1,10 @@
+export const Button = () => {
+  return (
+    <button
+      type="button"
+      className="rounded-sm bg-indigo-600 px-2 py-1 text-xs font-semibold text-white shadow-xs hover:bg-indigo-500 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+    >
+      Button text
+    </button>
+  )
+}
