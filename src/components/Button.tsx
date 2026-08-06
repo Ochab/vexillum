3bee4ca86a0465bf1a696dd04952757afd745b05
Ochab <greg@ochab.us>
2026-08-06 15:51:27 -0500

    Update Button component

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,10 +1,20 @@
-export const Button = () => {
+interface ButtonProps {
+  label?: string
+  variant?: 'solid' | 'outline'
+}
+
+const variantStyles = {
+  solid: 'bg-button-solid-surface hover:bg-button-solid-surface-hover text-button-solid-content',
+  outline: 'bg-button-outline-surface hover:bg-button-outline-surface-hover text-button-outline-content'
+}
+
+export const Button = ({ label = 'Label', variant = 'solid'}: ButtonProps) => {
   return (
     <button
       type="button"
-      className="rounded-sm bg-indigo-600 px-2 py-1 text-xs font-semibold text-white shadow-xs hover:bg-indigo-500 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+      className={`rounded-sm px-3 py-2 text-sm font-semibold shadow-xs font-semibold shadow-xs dark:shadow-none focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600 ${variantStyles[variant]}`}
     >
-      Button text
+      { label }
     </button>
   )
 }
